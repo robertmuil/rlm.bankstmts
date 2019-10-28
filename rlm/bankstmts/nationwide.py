924546175231ceb627a2eec8b0a7bbf6fef5a35a
Robert Muil <robertmuil@gmail.com>
2019-10-28 15:27:24 +0000

    handle all-null columns

--- rlm/bankstmts/nationwide.py
+++ rlm/bankstmts/nationwide.py
@@ -8,6 +8,12 @@ log = logging.getLogger(__name__)
 
 
 def read_tx_file(fpath):
+    """
+    Read a single file of transactions, stripping currency signs out and converting numeric columns to numeric data
+    types.
+    :param fpath: path to transaction file.
+    :return: pd.DataFrame
+    """
     df = pd.read_csv(fpath,
                      infer_datetime_format=True,
                      parse_dates=[0],
@@ -20,7 +26,10 @@ def read_tx_file(fpath):
     }, inplace=True)
     log.info('    - read {:d} transactions from \'{}\''.format(len(df), fpath))
     for c in ['out', 'in', 'balance']:
-        df[c] = df[c + '_str'].str.replace(r'[^-+\d.]', '').astype(float)  # decimal.Decimal)
+        if df[c + '_str'].isnull().all():
+            df[c] = pd.np.nan
+        else:
+            df[c] = df[c + '_str'].str.replace(r'[^-+\d.]', '').astype(float)  # decimal.Decimal)
 
     return df.drop(['out_str', 'in_str', 'balance_str'], axis='columns')
 
