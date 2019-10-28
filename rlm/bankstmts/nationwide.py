f6aa5f2aeffa6cf7ca0b21a008ba66f54b50c513
Robert Muil <robertmuil@gmail.com>
2019-10-28 15:08:00 +0000

    clean up code a bit

--- rlm/bankstmts/nationwide.py
+++ rlm/bankstmts/nationwide.py
@@ -2,24 +2,26 @@ import pandas as pd
 import warnings
 import glob
 
-gumpf_rows=4
+gumpf_rows = 4
+
 
 def read_tx_file(fpath):
     df = pd.read_csv(fpath,
-                 infer_datetime_format=True,
-                 parse_dates=[0],
-                 skiprows=gumpf_rows,
-                 encoding='latin1').rename(columns=
-                    {
-                        'Paid out': 'out_str',
-                        'Paid in':  'in_str',
-                        'Balance':  'balance_str',
-    })
+                     infer_datetime_format=True,
+                     parse_dates=[0],
+                     skiprows=gumpf_rows,
+                     encoding='latin1')\
+    df.rename(columns={
+        'Paid out': 'out_str',
+        'Paid in': 'in_str',
+        'Balance': 'balance_str',
+    }, inplace=True)
     print('read {:d} transactions from \'{}\''.format(len(df), fpath))
     for c in ['out', 'in', 'balance']:
-        df[c] = df[c+'_str'].str.replace(r'[^-+\d.]', '').astype(float)#decimal.Decimal)
-    
+        df[c] = df[c + '_str'].str.replace(r'[^-+\d.]', '').astype(float)  # decimal.Decimal)
+
     return df.drop(['out_str', 'in_str', 'balance_str'], axis='columns')
+
 
 def read_tx_files(fpath_pattern):
     df_list = []
@@ -28,7 +30,7 @@ def read_tx_files(fpath_pattern):
         df = read_tx_file(fpath)
         df['fpath'] = fpath
         df_list += [df]
-    
+
     if len(df_list) < 1:
         warnings.warn('no files found with pattern \'{}\''.format(fpath_pattern))
         df = None
@@ -44,14 +46,15 @@ def read_tx_files(fpath_pattern):
         ))
     return df
 
+
 def read_tx_accounts(fpath_patterns):
     df_list = []
-    for acct,fpath_pattern in fpath_patterns.items():
+    for acct, fpath_pattern in fpath_patterns.items():
         df = read_tx_files(fpath_pattern)
         if df is not None:
             df['account'] = acct
             df_list += [df]
-    
+
     if len(df_list) > 0:
         df = pd.concat(df_list).sort_values('Date')
         print('read {:d} transactions from {:d} accounts: from {} to {}'.format(
@@ -62,5 +65,5 @@ def read_tx_accounts(fpath_patterns):
         ))
     else:
         df = None
-    
+
     return df
